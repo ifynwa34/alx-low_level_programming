e69e188359e79a8ef2bcb7151dec06a6af5573d2
ifynwa34 <ifyigbah34@gmail.com>
2023-08-11 13:31:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main entry point to the c programm. Using the printf function to display sizes
- * Return: 0 (Success)
+ * main -  Using the printf function to display sizes
+ * Return: always 0 (Success)
  */
 
 int main(void)
@@ -11,12 +11,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float f;
+	float e;
 
-printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
-printf("sizes of an int: %lu byte(s)\n", (unsigned long) sizeof(b));
-printf("sizes of a long int: %lu byte(s)\n", (unsigned long) sizeof(c));
-printf("sizes of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
-printf("sizes of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+printf("size of a char: %lu byte(s)\n", sizeof(a));
+printf("sizes of an int: %lu byte(s)\n", sizeof(b));
+printf("sizes of a long int: %lu byte(s)\n", sizeof(c));
+printf("sizes of a long long int: %lu byte(s)\n", sizeof(d));
+printf("sizes of a float: %lu byte(s)\n" sizeof(e));
 return (0);
 }
