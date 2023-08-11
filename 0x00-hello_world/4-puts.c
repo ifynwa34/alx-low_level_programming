c8e7c9b4c40607bec89b47a2e6141b4d343a0149
ifynwa34 <ifyigbah34@gmail.com>
2023-08-10 21:09:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the c programme demonstrating the use of puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
