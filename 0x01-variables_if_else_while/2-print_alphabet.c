f39aea508ee5cfe8b5c5c4ca93d2c054dcf936ee
ifynwa34 <ifyigbah34@gmail.com>
2023-08-13 06:15:37 -0700

    2-print_alphhabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  Entry point to printing alphabet with putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char w;
+
+	w = 'a';
+
+	while
+		(w <= 'z')
+		{ putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
