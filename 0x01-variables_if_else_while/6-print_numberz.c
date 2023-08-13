667e2ba9a4366db454213b71478650ee62df88b7
ifynwa34 <ifyigbah34@gmail.com>
2023-08-13 06:54:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point printing numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int g;
+for (g = 0 ; g < 10 ; g++)
+putchar(g + '0');
+putchar('\n');
+return (0);
+}
